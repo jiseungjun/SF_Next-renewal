7a082852a5d1966b28f0f813eea6c3ed56b17f3b
9Diin <weartstudio@naver.com>
2024-11-25 17:41:57 +0900

    🔥 [feat] 회원가입 페이지 UI 개발 완료

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import Link from "next/link";
 import { useState } from "react";
+import Link from "next/link";
 import { Button, Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter, Label, Input } from "@/components/ui";
 import { Eye, EyeOff } from "@/public/assets/icons";
 
