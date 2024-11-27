5650afb2b4cb60d85541dfdccb6d9ba8b6698b70
9Diin <weartstudio@naver.com>
2024-11-27 17:20:26 +0900

    🔥[refactor] 유저별 게시물 조회 권한 - 미들웨어 기능 추가

--- middleware.ts
+++ middleware.ts
@@ -4,11 +4,13 @@ import { createClient } from "./lib/supabase/client";
 
 const getTask = async (taskId: number, userId: string) => {
     const supabase = createClient();
-    // 사용자의 ID를 기준으로 task를 조회
-    const { data, status, error } = await supabase.from("tasks").select("*").eq("id", taskId).eq("user_id", userId); // 로그인된 사용자의 task만 조회
+    /** 사용자 ID를 기준으로 TASK를 조회 */
+    /** 여기서 userId는 로그인한 계정의 user_id */
+    const { data, status, error } = await supabase.from("tasks").select("*").eq("user_id", userId).eq("id", taskId);
 
     if (data && status === 200) return data[0];
     if (error) throw error;
+
     return null;
 };
 
@@ -28,25 +30,29 @@ export async function middleware(request: NextRequest) {
         return NextResponse.redirect(new URL("/", request.url));
     }
 
-    /** 사용자가 로그인이 된 경우, /board/:path* 경로와 같은 콘텐츠 페이지 접근했을 시 로그인된 계정의 task만 조회 */
-    // 로그인된 사용자가 특정 task를 조회하려는 경우, task ID 확인
+    /** 사용자가 로그인이 된 경우, /board/:path* 경로와 같은 콘텐츠 페이지에 접근했을 시
+     * 로그인된 유저의 task가 아니면 not-found 페이지로 리다이렉트
+     *
+     * 1. 로그인된 사용자가 특정 task를 조회하려는 경우, task의 id 확인
+     */
+
     if (request.nextUrl.pathname.startsWith("/board/")) {
         const taskId = parseInt(request.nextUrl.pathname.split("/board/")[1], 10);
 
-        // taskId가 유효하지 않으면 에러 처리
+        // 1. taskId가 유효하지 않으면 에러 처리
         if (isNaN(taskId)) {
-            return NextResponse.redirect(new URL("/", request.url)); // 잘못된 taskId로 접근할 때 홈으로 리다이렉트
+            return NextResponse.redirect(new URL("/", request.url)); // 잘못된 taskId로 접근할 때, 로그인 페이지로 리다이렉트
         }
 
-        // 사용자가 로그인 상태인 경우 해당 task가 사용자의 것인지 확인
+        // 2. 사용자가 로그인 상태인 경우, 해당 task가 본인의 것인지 체크
         if (user) {
             const task = await getTask(taskId, JSON.parse(user.value).id); // 로그인된 사용자의 task만 조회
+
             if (!task) {
-                return NextResponse.redirect(new URL("/not-found", request.url)); // 사용자가 해당 task에 접근할 수 없으면 /board로 리다이렉트
+                return NextResponse.redirect(new URL("/not-found", request.url)); // 사용자가 해당 task에 접근할 수 없으면 /not-found로 리다이렉트
             }
         }
     }
-
     /** 기본적인 요청을 그대로 진행 */
     return NextResponse.next();
 }
